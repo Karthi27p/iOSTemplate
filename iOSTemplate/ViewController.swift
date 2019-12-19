8b43609de1b32e65bc239a973a93a3899e622459
TringKarthi <karthi.p@tringapps.com>
2019-12-19 22:05:03 +0530

    Initial Commit

---
+++ iOSTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTemplate
+//
+//  Created by Karthi on 19/12/19.
+//  Copyright © 2019 Karthi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
